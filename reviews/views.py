6db38df758700ffd0b56a9afa5b08a366839bd35
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2024-03-12 10:26:34 +0300

    handling form submission and data extton

--- reviews/views.py
+++ reviews/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 def review(request):
+    """view function to handle the form"""
     # pass
+    if request.method  == 'POST':
+        entered_username = request.POST['username']
+        print(entered_username)
+        return HttpResponseRedirect("/thank-you")
+
     return render(request, "reviews/review.html")
+
+
+def thank_you(request):
+    return render(request,"reviews/thank_you.html" )
